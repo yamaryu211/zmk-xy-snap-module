d908da8eefe2d2005bd4f93d06ee476ea6026f79
yamaryu211 <yamaryu380@gmail.com>
2025-06-28 18:02:44 +0800

    しゅうせい

--- src/zmk_xy_snap_input_processor.c
+++ src/zmk_xy_snap_input_processor.c
@@ -1,7 +1,5 @@
 #include <zephyr/kernel.h>
 #include <zephyr/device.h>
-#include <zmk/events.h>
-#include <zmk/event_manager.h>
 #include <zmk/input/input.h>
 #include <zmk/input/input_listener.h>
 #include <zmk/input/input_processor.h>
